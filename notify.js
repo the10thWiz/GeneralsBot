89fb1151a0e588dfdb6ac69f57c9e963e232896e
the10thWiz <mpomes@wi.rr.com>
2017-02-15 09:59:20 -0600

    Fix notify.js

--- notify.js
+++ notify.js
@@ -1,4 +1,3 @@
-
 Notification.requestPermission();
 var notify = {
     icon:'https://cdn.discordapp.com/icons/261627238972129283/00255c253dd9da1cf8c8e0e113aaa350.png',
@@ -7,9 +6,8 @@ var notify = {
     focus:function() {if(!document.hasFocus())window.open("", "").close();},
     cur:null,
     curId:0,
-    visible:true,
     new:function(title, message, tag) {
-        if(!this.visible) {
+        if((document.hidden===true || document.msHidden===true || document.webkitHidden===true || !document.hasFocus())) {
             this.cur = new Notification(title, {
                 body:message,
                 icon:this.icon,
@@ -23,24 +21,3 @@ var notify = {
         }
     },
 };
-document.addEventListener("visibilitychange", function() {
-    if (document.hidden === false) {
-        notify.visible = false;
-    } else {
-        notify.visible = true;
-    }
-}, false);
-document.addEventListener("msVisibilitychange", function() {
-    if (document.msHidden === false) {
-        notify.visible = false;
-    } else {
-        notify.visible = true;
-    }
-}, false);
-document.addEventListener("webkitVisibilitychange", function() {
-    if (document.webkitHidden === false) {
-        notify.visible = false;
-    } else {
-        notify.visible = true;
-    }
-}, false);
